0dfad6b9889b507fc2e00e222399e2f59435623b
igor-tkachuk-03 <157903990+igor-tkachuk-03@users.noreply.github.com>
2024-03-04 20:20:29 +0200

    Update Main.tf

--- Home2/Main.tf
+++ Home2/Main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIA5LPSYQR6CY6FJN7A"
-  secret_key = "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"
+  access_key = "88888"
+  secret_key = "88888888888"
   region     = "eu-central-1"
 }
